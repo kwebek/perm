25a64ae2d9a067c4702f9dc7b09e2e3c257eb991
kwebek <kwk-studio@yandex.ru>
2020-07-10 18:01:12 +0400

    Start v 0.1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="Ru">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Alexander Machnev - Adaptive Feedback Form</title>
+</head>
+
+<body>
+	<div>
+		PHP here 4:
+	<?php
+	echo "Hello Bob!";
+	$x="345345";
+		echo "string";
+		echo $x;
+	?>
+	</div>
+</body>
+
+</html>
